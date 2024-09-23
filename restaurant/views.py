0a5c2b64d60bbdfdf740462868fce5e478c9911a
Stefan-Phan <your_email@adress.com>
2024-09-23 17:34:21 +0930

    add viewset and booking api

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from .models import Menu, Booking
 from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_list_or_404
+from rest_framework import status, viewsets
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -17,7 +17,7 @@ class MenuItemView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = MenuSerializer(request.data)
+        serializer = MenuSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -25,12 +25,12 @@ class MenuItemView(APIView):
 
 class SingleMenuItemView(APIView):
     def get(self, request, id):
-        item = get_list_or_404(Menu, pk=id)
+        item = get_object_or_404(Menu, pk=id)
         serializer = MenuSerializer(item)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, id):
-        item = get_list_or_404(Menu, pk=id)
+        item = get_object_or_404(Menu, pk=id)
         serializer = MenuSerializer(item, data=request.data)
 
         if serializer.is_valid():
@@ -39,6 +39,11 @@ class SingleMenuItemView(APIView):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
-        item = get_list_or_404(Menu, pk=id)
+        item = get_object_or_404(Menu, pk=id)
         item.delete()
         return Response(status=status.HTTP_200_OK)
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
